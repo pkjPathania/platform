0c0e19e0e6761b3b3f871745d472399477923ba6
pankajpathania <ppathania67@gmail.com>
2026-06-29 20:46:03 +0530

    Initial platform structure

---
+++ platform/microservices/org/src/main/java/com/pankaj/platform/org/OrgApplication.java
@@ -0,0 +1,12 @@
+package com.pankaj.platform.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OrgApplication.class, args);
+  }
+}
